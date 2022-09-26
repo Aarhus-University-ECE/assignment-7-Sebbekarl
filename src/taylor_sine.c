c35431dce72ce278892a0a2068cc0ab11a81a8c3
sebbekarl <sebbekarl97@gmail.com>
2022-09-26 16:15:29 +0200

    test

--- src/taylor_sine.c
+++ src/taylor_sine.c
@@ -1,8 +1,45 @@
 #include "taylor_sine.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <math.h>
+
+double factorial(double num){
+    double res = 1;
+    for(int i = 1; i<num+1; i++){
+        res *= i;
+    }
+    return res;
+}
+
+bool check_pre(double num,double num2, int n){
+    double pre = pow(10, -n);
+
+    printf("%f", fabs(num-num2));
+
+    if(fabs(num-num2) > pre){
+        return true;
+        printf("true");
+    }else{
+        return false;
+    }
+
+
+}
 
 double taylor_sine(double x, int n)
 {
-    /* implement your function here */
+    double result = x;
+    double oldres = 0;
+    //printf("%lf test", result);
+
+    for(int i; !check_pre(result,oldres,n); i++){
+        
+        oldres = result;
+        result += (pow(x,2*i+1)/factorial(2*i+1));
+        //printf("%lf",result);
+
+    }
+
     
-    return -1.0;
+    return result;
 }
